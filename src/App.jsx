454fc776c808f98ebc76efcb664e00ee7c5b00dd
wahida <wahidacse280@gmail.com>
2024-05-25 23:23:04 +0600

    folder added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/home/Footer"
+import Navbar from "./components/home/Navbar"
+import Home from "./pages/Home"
+
+function App() {
+ return (
+    <>
+     <Navbar />
+     <Home />
+     <Footer/>
+      
+    
+    </>
+  )
+}
+
+export default App
